76282303a3d57d8ccf963fe75d80dddb55b6fe47
Peter Seibel <peter@gigamonkeys.com>
2017-10-07 12:30:53 -0700

    Namespace API urls and disallow punctuation in names.

--- app.py
+++ app.py
@@ -7,14 +7,20 @@ app.config['DEBUG'] = True
 redirects = {}
 redirects['goog'] = 'https://google.com'
 
-@app.route("/API/create/", methods=['POST'])
+@app.route("/$create$/", methods=['POST'])
 def create():
     redirects[request.form['name']] = request.form['url']
     return redirect('/' + request.form['name'], code=302)
 
+
 @app.route("/<name>")
 def redirection(name):
+    name = depunctuate(name)
     if name in redirects:
         return redirect(redirects[name], code=307)
     else:
         return render_template('missing.html', name=name)
+
+
+def depunctuate(name):
+    return ''.join(filter(str.isalnum, name))
