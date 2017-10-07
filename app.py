0143ff4eb44708f819d2860cdc2cf1a85bd34c08
Peter Seibel <peter@gigamonkeys.com>
2017-10-07 13:30:04 -0700

    Fragile but handles different patterns for different number of args.

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
+from collections import defaultdict
 from flask import Flask, redirect, render_template, request
+import re
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-redirects = {}
-redirects['goog'] = 'https://www.google.com/search?q={}'
-
+redirects = defaultdict(dict)
+redirects['goog'][0] = 'https://www.google.com/'
+redirects['goog'][1] = 'https://www.google.com/search?q={}'
 
 @app.route("/$create$/", methods=['POST'])
 def create():
-    redirects[request.form['name']] = request.form['url']
+    name    = request.form['name']
+    pattern = request.form['url']
+    redirects[name][count_args(pattern)] = pattern
     return redirect('/' + request.form['name'], code=302)
 
 
@@ -20,10 +24,14 @@ def redirection(name, rest):
     name = depunctuate(name)
     if name in redirects:
         args = rest.split('/') if rest else []
-        return redirect(redirects[name].format(*args), code=307)
+        return redirect(redirects[name][len(args)].format(*args), code=307)
     else:
         return render_template('missing.html', name=name)
 
 
 def depunctuate(name):
     return ''.join(filter(str.isalnum, name))
+
+
+def count_args(pattern):
+    return len(re.findall('{\d*}', pattern))
