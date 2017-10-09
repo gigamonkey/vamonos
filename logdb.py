0c5d9a5911b4931f89682301b3cb6733d170b9b0
Peter Seibel <peter@gigamonkeys.com>
2017-10-09 14:16:49 -0700

    Load and save on-disk cache as appropriate.

--- logdb.py
+++ logdb.py
@@ -1,11 +1,9 @@
 from collections import defaultdict
 from fcntl import LOCK_EX, LOCK_SH, LOCK_UN, flock
-from os import fsync
+from os import fsync, SEEK_SET, SEEK_END
 from time import sleep
+import json
 
-START   = 0
-CURRENT = 1
-END     = 2
 
 class Log:
 
@@ -15,7 +13,7 @@ class Log:
     def write(self, data):
         with open(self.file, mode='a') as f:
             flock(f, LOCK_EX)
-            f.seek(0, END)
+            f.seek(0, SEEK_END)
             print(data, file=f)
             f.flush()
             fsync(f.fileno())
@@ -25,27 +23,33 @@ class Log:
     def read(self, low_water_mark):
         with open(self.file, mode='r') as f:
             flock(f, LOCK_SH)
-            f.seek(low_water_mark, START)
+            f.seek(low_water_mark, SEEK_SET)
             while True:
                 line = f.readline()
                 pos = f.tell()
-                if line == '': break
-                yield (line[:-1], pos)
+                if line == '':
+                    break
+                yield line[:-1], pos
             flock(f, LOCK_UN)
 
 
 class LoggedDB:
 
-    def __init__(self, file, log):
-        self.file = file
-        self.log = log
-        self.lwm = 0
-        self.cache = defaultdict(dict)
+    def __init__(self, name):
+        self.file = name + '.data'
+        self.log = Log(name + '.log')
+        try:
+            self._load()
+        except:
+            self.low_water_mark = 0
+            self.cache = defaultdict(dict)
 
     def _log(self, *entry):
+        "Log data to our transaction log."
         self.log.write('\t'.join([str(x) for x in entry]))
 
     def _replay(self, entry):
+        "Replay a log entry to reflect it in our in-memory cache."
         verb, name, n, pattern = entry.split('\t')
         n = int(n)
         if verb == 'SET':
@@ -58,23 +62,49 @@ class LoggedDB:
             raise Error('Bad log entry: {}'.format(entry))
 
     def _refresh(self):
-        for (entry, lsn) in self.log.read(self.lwm):
+        "Replay any new log entries against our in-memory cache."
+        new_lwm = self.low_water_mark
+        for (entry, lsn) in self.log.read(self.low_water_mark):
             print('** REPLAYING {}'.format(entry))
             self._replay(entry)
-            self.lwm = lsn
+            new_lwm = lsn
+
+        if self.low_water_mark < new_lwm:
+            self.low_water_mark = new_lwm
+            self._save()
 
     def _load(self):
+        "Load cached data from disk so we don't have to replay the whole log."
         with open(self.file) as f:
+            flock(f, LOCK_EX)
+            print("** LOADING CACHE FROM DISK **")
             self.cache = defaultdict(dict)
-            for (name, patterns) in json.load(f).items():
+            data = json.load(f)
+            self.low_water_mark = data['low_water_mark']
+            for (name, patterns) in data['cache'].items():
                 for (n, pattern) in patterns.items():
                     self.cache[name][int(n)] = pattern
+            flock(f, LOCK_UN)
 
     def _save(self):
+        # We could check that our low water mark is greater than the
+        # one already on disk before we write since it's possible that
+        # someone else has read farther in the log than us and gotten
+        # in and written out their cache to disk. But it doesn't
+        # really matter since we never actually read from the on-disk
+        # cache at startup. Rolling the cache back in time will, at
+        # worst, make some processes have to reply a few more log
+        # records than they might have otherwise.
         with open(self.file, 'w') as f:
-            json.dump({'lwm': self.lwm, 'data': self.cache}, f)
+            flock(f, LOCK_EX)
+            print("** SAVING CACHE TO DISK **")
+            json.dump({
+                'low_water_mark': self.low_water_mark,
+                'cache': self.cache
+            }, f)
+            flock(f, LOCK_UN)
 
-    # Public methods
+    # Accessors -- must check for new entries in log.
 
     def has_name(self, name):
         self._refresh()
@@ -92,6 +122,8 @@ class LoggedDB:
         self._refresh()
         return self.cache[name][n]
 
+    # Mutators -- write entries to log.
+
     def delete_name(self, name):
         self._log('DELETE_NAME', name, '*', '*')
 
@@ -104,22 +136,18 @@ class LoggedDB:
 
 if __name__ == '__main__':
 
-
-    if False:
-        print('Opening log')
-        log = Log('test2.log')
-        lsns = [ log.write(x) for x in ['a', 'b', 'c', 'def', 'ghij', 'kl', 'mnopqst'] ]
-        print(lsns)
-        for (record, lsn) in log.read(lsns[2]):
-            print('{}: {}'.format(lsn, record))
-
     from sys import argv
 
-    db = LoggedDB("testdb.data", Log("testdb.log"))
+    db = LoggedDB('testdb')
 
     if argv[1] == 'reader':
         while True:
             print(db.get_pattern('foo', 0))
             sleep(2.0)
+
     elif argv[1] == 'writer':
         db.set_pattern('foo', 0, argv[2])
+
+    elif argv[1] == 'dump':
+        for (e, s) in db.log.read(0):
+            print('{}\t{}'.format(e, s))
